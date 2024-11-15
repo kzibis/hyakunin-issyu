225eab3b27466ede53f70a9fbfe6bc334f44558f
kzibis <100402859+kzibis@users.noreply.github.com>
2024-11-15 22:56:09 +0900

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,42 @@ const inter = Yuji_Syuku({
   weight: "400"
 })
 
+const siteName= "百人一首JAPAN";
+const description = "Search the English translation of the Japanese poem 「hyakunin-issyu」into English.";
+const url = 'https://hyakunin-issyu.vercel.app/';
+const imgUrl = '/favicon.ico'
+const keywords = ["百人一首", "hyakunin-issyu", "和歌", "Japanese", "English", "translation", "英訳"];
+
 export const metadata: Metadata = {
-  title: "百人一首JAPAN",
-  description: "Search the English translation of the Japanese poem 「hyakunin-issyu」into English.",
+  title: siteName,
+  description: description,
+  keywords: keywords,
+  icons: imgUrl,
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    locale: 'ja_JP',
+    type: 'website',
+    images: [{
+      url: imgUrl
+    }]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteName,
+    description,
+    images: imgUrl
+    // site: '@サイト用アカウントのTwitterID',
+    // creator: '@作者のTwitterID',
+  },
+  verification: {
+    // google: 'Fjx4p3cOWCdJJOsx9Cz11hOS0a9gj763H1vCqp9QoZs',
+  },
+  alternates: {
+    canonical: url,
+  },
 };
 
 export default function RootLayout({
