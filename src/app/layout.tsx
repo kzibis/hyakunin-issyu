7f4629da08602fb5a720bc2e28175fef2702c48e
kzibis <100402859+kzibis@users.noreply.github.com>
2025-01-04 11:59:29 +0900

    updatae

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
         <NextUIProvider>
           {children}
         </NextUIProvider>
-        <Analytics></Analytics>
+        <Analytics/>
       </body>
     </html>
   );
